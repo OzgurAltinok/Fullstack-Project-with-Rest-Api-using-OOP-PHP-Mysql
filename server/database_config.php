47ba9c64a954817b70be630255af3c670e75808c
zgrcnltnk <ozgurcanaltinok@gmail.com>
2019-04-22 19:38:22 +0300

    receiving data from mysql database with get request works

--- server/database_config.php
+++ server/database_config.php
@@ -1,7 +1,6 @@
 <?php
 class Database
 {
-
     private $host = "localhost";
     private $db_name = "example";
     private $username = "root";
@@ -20,5 +19,31 @@ class Database
         }
 
         return $this->conn;
+    }
+
+    public function getDataFromTable()
+    {
+        $con=mysqli_connect("localhost", "root", "", "example");
+        // Check connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        // Perform queries
+        $result = mysqli_query($con, "SELECT * FROM mytable");
+        // mysqli_query($con,"INSERT INTO Persons (FirstName,LastName,Age)
+        // VALUES ('Glenn','Quagmire',33)");
+
+        $arr = array();
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            array_push($arr, array(
+                'sku' => $row['sku'],
+                'name' => $row['name'],
+                'price' => $row['price'],
+            ));
+        }
+        return $arr;
+        mysqli_close($con);
     }
 }
--- server/index.php
+++ server/index.php
@@ -9,11 +9,13 @@ include_once "./models/Product.php";
 
 $database = new Database();
 $db = $database->getConnection();
+$myData = $database->getDataFromTable();
 
 $productExample = new Product("123", 456, "ozgur");  
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-     echo json_encode($productExample->printAsJson());
+     //  echo json_encode($productExample->printAsJson());
+     echo json_encode($myData);
 }
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
      echo json_encode("post");
