6dfb510f4fc8ac177b9a59968d7ec38340364549
zgrcnltnk <ozgurcanaltinok@gmail.com>
2019-04-26 14:57:22 +0300

    delete method added, tested and client redesigned

--- README.md
+++ README.md
@@ -16,3 +16,12 @@
 14. Design[+]
 15. Use Get & post request with axios[+]
 16. Print[+]
+
+## Instructions
+
+1. In the /server folder open Cmd and write "php -S localhost:8000" to run
+2. Install live-server package with npm "npm install -g live-server"
+3. In the /client folder open Cmd and write "live-server" to run
+4. Open xampp or wamp for the project working on local
+5. Firstly, you can go to the new product page and add some products
+4. That is it :)
--- server/index.php
+++ server/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once "./database_config.php";
+include_once "./database.php";
 include_once "./models/Product.php";
 include_once "./models/Book.php";
 include_once "./models/Dvddisc.php";
@@ -8,7 +8,6 @@ include_once "./models/Furniture.php";
 
 
 $database = new Database();
-// $db = $database->getConnection();
 
 $database->enableCorsAttack();
 
@@ -16,65 +15,68 @@ $database->enableCorsAttack();
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
   // Request to get the data added as a json
-  $database->getRequest();    
+    $database->getRequest();
 
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // Pull the posted json 
+    // Pull the posted json
+    $content = file_get_contents('php://input', true);
+
+    //If data is okay
+    if ($content !== false) {
+        //  Decode the content data
+        $json = json_decode($content, true);
+
+        if (array_key_exists("weight", $json)) {
+
+               // Create book obj if you send Weight attribute
+            $book = new Book($json['sku'], $json['name'], $json['price'], $json['weight']);
+
+            // Insert it in Book table
+            $database->insertDataToTable($book->getSku(), $book->getName(), $book->getPrice(), $book->getWeight(), "weight", "book");
+        } elseif (array_key_exists("size", $json)) {
+
+               // Create book obj if you send Size attribute
+            $dvddisc = new Dvddisc($json['sku'], $json['name'], $json['price'], $json['size']);
+
+            // Insert it in Dvddisc table
+            $database->insertDataToTable($dvddisc->getSku(), $dvddisc->getName(), $dvddisc->getPrice(), $dvddisc->getSize(), "size", "dvddisc");
+        } elseif (array_key_exists("dimensions", $json)) {
+
+               // Create book obj if you send Dimensions attribute
+            $furniture = new Furniture($json['sku'], $json['name'], $json['price'], $json['dimensions']);
+
+            // Insert it in Furniture table
+            $database->insertDataToTable($furniture->getSku(), $furniture->getName(), $furniture->getPrice(), $furniture->getDimensions(), "dimensions", "furniture");
+        }
+
+        // Request to get the data added just now as a json
+        $database->getRequest();
+
+    } else {
+        error_log("Failed to Post Data!");
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+    // Get posted object via axios
     $content = file_get_contents('php://input', true);
 
     //If data is okay
     if ($content !== false) {
 
-        //  Decode the content data
         $json = json_decode($content, true);
 
-        if (array_key_exists("weight", $json)) {
-             
-               // Create book obj if you send Weight attribute
-             $book = new Book($json['sku'], $json['name'], $json['price'], $json['weight']); 
-
-               // Insert it in Book table
-             $database->insertDataToTable($book->getSku(), $book->getName(), $book->getPrice(), $book->getWeight(), "weight", "book");
-
-        }elseif(array_key_exists("size", $json)){
-
-               // Create book obj if you send Size attribute
-             $dvddisc = new Dvddisc($json['sku'], $json['name'], $json['price'], $json['size']); 
-
-               // Insert it in Dvddisc table
-             $database->insertDataToTable($dvddisc->getSku(), $dvddisc->getName(), $dvddisc->getPrice(), $dvddisc->getSize(), "size" ,"dvddisc");
-
-        }elseif(array_key_exists("dimensions", $json))
-        {
-
-               // Create book obj if you send Dimensions attribute
-             $furniture = new Furniture($json['sku'], $json['name'], $json['price'], $json['dimensions']); 
-
-             // Insert it in Furniture table
-             $database->insertDataToTable($furniture->getSku(), $furniture->getName(), $furniture->getPrice(), $furniture->getDimensions(), "dimensions" ,"furniture");
-          
+        //Identify which kind of data is came
+        if ($json['handlerType'] == "KG") {
+            $myTableName = "BOOK";
+        } elseif ($json['handlerType'] == "MB") {
+            $myTableName = "DVDDISC";
+        } elseif ($json['handlerType'] == "Dimensions") {
+            $myTableName = "FURNITURE";
         }
-        else
-        {
-          // Decode the content data
-          $json = json_decode($content, true);
 
-          // Create a new Product instance
-          $productExample = new Product1($json['sku'], $json['name'], $json['price']);
-          
-          // Insert it in db table
-          $database->insertDataToTable2($productExample->getSku(), $productExample->getName(), $productExample->getPrice());
-
-        }        
-
-        // Request to get the data added just now as a json
-        $database->getRequest();
-    } else {
-        error_log("Failed to Post Data!");
+        //Delete an instance from the specific table
+        $database->deleteDataFromTable($json['sku'], $myTableName);
+        
     }
-}
-
-elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-// not yet
 }
